cae1f927d4f3005da1baf55b0f6e263811cb2d10
vibek44 <kshetrivibek@gmail.com>
2021-03-17 20:15:18 +0200

    index

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express()
 
 app.use(cors())
 
+app.use(express.static('build'))
+
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :bo'))
@@ -36,9 +38,7 @@ let persons = [
   
 ]
 
-app.get('/', (request, response) => {
-  response.send('hello phonebook321')
-})
+
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
