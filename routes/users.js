0cae0dda82c76678bca4e6d449df8953f7009b6b
vibek44 <kshetrivibek@gmail.com>
2022-01-01 20:10:59 +0200

    adding error handler middleware

--- routes/users.js
+++ routes/users.js
@@ -1,82 +1,95 @@
-const express=require('express');
-const router=express.Router();
 
-let persons=[
-    { 
-       "id": 1,
-       "name": "Arto Hellas", 
-       "number": "040-123456"
-     },
-     { 
-       "id": 2,
-       "name": "Ada Lovelace", 
-       "number": "39-44-5323523"
-     },
-     { 
-       "id": 3,
-       "name": "Dan Abramov", 
-       "number": "12-43-234345"
-     },
-     { 
-       "id": 4,
-       "name": "Mary Poppendieck", 
-       "number": "39-23-6423122"
-     }
- ]
- 
+const express=require('express')
+const router=express.Router()
+const Person=require('../models/person')
+
 
 router.get('/',(req,res,next)=>{
-         if(req.baseUrl==='/api/persons'){
-        res.json(persons);
-         }
-       next();
-  })
-  
-router.get('/', (req,res,next)=>{
-  if(req.baseUrl==='/info'){
-     res.send(`<p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>`)
-    }
-  next();
+  if(req.baseUrl==='/api/persons'){
+    Person.find({}).then(result=>{
+     res.json(result)
+    })
+   }
+  next()
 })
-  
+
+router.get('/', (req,res)=>{
+
+  if(req.baseUrl==='/info'){
+     Person.find({}).then(result=>{
+      res.send(`<p>Phonebook has info for ${result.length} people</p>
+      <p>${new Date()}</p>`)
+    })
+  }
+})
+
 router.get('/:id', (req,res,next)=>{
-   const id=Number(req.params.id);
-   const person=persons.find((person)=>person.id===id);
-   if(!person){
-     res.status(404).end('content not found!!');
-     return;
-    }
-    res.json(person);
-    next();
+  Person.findById(req.params.id)
+    .then(result=>{
+           if(result){
+             res.json(result)
+          }else{
+           res.status(404).send('404! not found')
+           }
+    })
+    .catch(error=>{
+      next(error) 
+    })
   })
-  
-  const generateId=()=>Math.floor(Math.random()*100+persons.length)
-  
-  router.post('/',(req,res)=>{ 
-    const body=req.body;
-    if(!(body.name && body.number)){
-      res.status(400).json({error:'Name or number is missing'})
-      return;
-    }
-   /* let person=persons.find((person)=>{
-      return person.name.toLowerCase()===body.name.trim().toLowerCase()})
-      if(person){
-        res.status(400).json({error:'Name must be unique'})
-        return;
-      }*/
-    
-    const id=generateId();
-    person={id:id,...body}
-    persons=persons.concat(person)
-    res.status(201).json(person)
-  
+
+router.post('/',(req,res)=>{ 
+  const body=req.body
+
+  if(!(body.name && body.number)){
+    res.status(400).json({error:'Name or number is missing'})
+    return;
+  }
+
+  const person = new Person({
+   name:body.name,
+   number:body.number
   })
-  
-  router.delete('/:id', (req,res)=>{
-    let id=Number(req.params.id);
-    persons=persons.filter((person)=>person.id!==id);
-    res.status(204).end();
+
+  person.save().then(savedNote => {
+    res.json(savedNote)    
   })
+})
+
+router.put('/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedNote => {
+      res.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+router.delete('/:id', (req,res)=>{
+    Person.findByIdAndDelete(req.params.id)
+    .then(result=>
+      res.status(204).end()
+    )
+    .catch(error=>next(error)) 
+  })
+
+  
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send( 'error : malformatted id' )
+    } 
+  next(error)
+
+  }
+  
+  // this has to be the last loaded middleware.
+  router.use(errorHandler)
 
 module.exports=router
