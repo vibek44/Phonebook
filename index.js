7bab080d55514ed12f1c720a326705e2662bd62a
vibek44 <kshetrivibek@gmail.com>
2022-01-04 18:53:38 +0200

    adding eslint for static code analysis

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
-const express=require('express');
+require('dotenv').config()
+const express=require('express')
 const cors=require('cors')
-const morgan=require('morgan');
+const morgan=require('morgan')
 const personRouter=require('./routes/users')
-const app=express();
-app.use(express.json());
+
+
+const app=express()
+app.use(express.json())
 
 app.use(cors())
 
 
 
-morgan.token('token',(req,res)=>{
-  return JSON.stringify(req.body)
+morgan.token('token',(req) => {
+    return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :token'))
@@ -19,12 +21,12 @@ app.use(express.static('build'))
 app.use(['/api/persons','/info'], personRouter)
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+    res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 
-const PORT=process.env.PORT || 3001
+const PORT =   process.env.PORT || 3001
 
-app.listen(PORT,()=>console.log('Server is running on port 3001'));
+app.listen(PORT,() => console.log('Server is running on port 3001'))
