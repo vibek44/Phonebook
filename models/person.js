091cc34591a51d317ddc4211d4e64be270958911
vibek44 <kshetrivibek@gmail.com>
2022-01-06 18:04:11 +0200

    removing mongoose connection to app.js file

--- models/person.js
+++ models/person.js
@@ -1,27 +1,34 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,useUnifiedTopology:true})
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
 
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number:Number
+    name: {
+        type:String,
+        required:[true,'name is required'],
+        unique:[true,'name must be unique'],
+        minlength:[3,'name length is short']
+    },
+    number:{
+        type:Number,
+        required:true,
+        validate:function(value){
+            if(!(/\d{9}/.test(value))){
+                throw new Error('invalid phone number')
+            }}
+    }
 })
+
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 module.exports= mongoose.model('Person', personSchema)
 
